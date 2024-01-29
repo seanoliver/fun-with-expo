517c2ccc1cf7252b09403b8c5d38a7ef869deced
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2024-01-29 00:08:07 -0800

    init

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi, sup</Text>
+      {/* Sign in Button: */}
+      <Button
+        title="Sign in"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
       <StatusBar style="auto" />
     </View>
   );
